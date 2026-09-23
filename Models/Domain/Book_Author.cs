79fcd069fa1be2caa19535cb79666ca2a3472681
ThuyXuan911 <thuynguyen091106@gmail.com>
2026-09-23 16:24:25 +0700

    First commit

---
+++ Models/Domain/Book_Author.cs
@@ -0,0 +1,13 @@
+﻿using Models.Domain;
+
+namespace WebAPI.Models.Domain
+{
+	public class Book_Author
+	{
+		public int Id { get; set; }
+		public int BookId { get; set; }
+		public Book Book { get; set; }
+		public int AuthorId { get; set; }
+		public Author Author { get; set; }
+	}
+}
